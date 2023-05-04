d58684508402d90fb3251ca663204462515fb73c
kubajznicek <kubajznicek0@gmail.com>
2023-05-04 08:28:12 +0200

    chat prikazy pres /tell

--- minecraft_bot.py
+++ minecraft_bot.py
@@ -10,6 +10,7 @@ bot = mineflayer.createBot({
   'port': 25565,
   'username': BOT_USERNAME,
   'hideErrors': False,
+  'logErrors': True,
   'version': '1.18.2'
 })
 
@@ -21,16 +22,29 @@ def handle(*args):
   movements = pathfinder.Movements(bot)
   print("I spawned 👋")
 
-  @On(bot, 'chat')
+  @On(bot, 'whisper')
   def handleMsg(this, sender, message, *args):
     print("Got message", sender, message)
     if message[0] == '!':
+      
+      if 'test' in message:
+        # print(bot.players[sender].entity.position)
+        # bot.lookAt(bot.players[sender].entity.position, True)
+        print(bot.timeou)
+        print(bot.blockAt(bot.players[sender].entity.position.offset(0, -1, 0)).displayName)
+        print(bot.digTime(bot.blockAt(bot.players[sender].entity.position.offset(0, -1, 0))))
+        bot.dig(bot.blockAt(bot.entity.position.offset(0, -1, 0)))
+
+      if 'lol' in message:
+        bot.stopDigging()
+        bot.players[BOT_USERNAME].gamemode = 'survival'
+        print(bot.players[BOT_USERNAME].gamemode)
 
       if 'come' in message:
         player = bot.players[sender]
         target = player.entity
         if not target:
-          bot.chat("I don't see you !")
+          bot.whisper(sender, "I don't see you !")
           return
 
         pos = target.position
@@ -40,20 +54,21 @@ def handle(*args):
       if 'mine' in message:
         cobble_pos = bot.findBlock({ 'matching': 12, 'maxDistance': 3, 'count': 1})    # id 12 je cobblestone
         if cobble_pos:
-          bot.chat("I found cobblestone at " + str(cobble_pos.position))
+          bot.whisper(sender, "I found cobblestone at " + str(cobble_pos.position))
+          # bot.lookAt(cobble_pos.position, True)
           while True:
             if bot.blockAt(cobble_pos.position).type == 12:
-              bot.dig(bot.blockAt(cobble_pos.position), True)
+              bot.dig(cobble_pos, True)
         else:
-          bot.chat("I don't see any cobblestone !")
+          bot.whisper(sender, "I don't see any cobblestone !")
 
       if 'inventory' in message:
         items = bot.inventory.items()
         items = list(map(lambda item: str(item.count) + ' ' + item.name, items))
         if items:
-          bot.chat("I have " + str(items))
+          bot.whisper(sender, "I have " + str(items))
         else:
-          bot.chat("I don't have anything")
+          bot.whisper(sender, "I don't have anything")
 
       if 'go to' in message:
         pos = {
